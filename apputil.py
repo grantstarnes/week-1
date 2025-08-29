b643ebf4991d9544776da04fbe97b16fec791385
grantstarnes <gstarnes@iu.edu>
2025-08-29 18:06:27 +0000

    Finished Exercise 1 and added function to apputil

--- apputil.py
+++ apputil.py
@@ -1,3 +1,13 @@
 
 
 # add code below ...
+
+# Exercise 1: Palindrome Function
+
+def palindrome (word):
+    word = word.replace(" ", "").lower()
+    word = word.replace(",", "")
+    return word == word[::-1]
+
+#Exercise 2: Balanced Parentheses
+
