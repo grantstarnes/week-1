37eb2aea910986bef346d4ded0148dc275c10e2f
grantstarnes <gstarnes@iu.edu>
2025-09-01 21:30:51 +0000

    Attempted Exercise 2 another way

--- apputil.py
+++ apputil.py
@@ -9,7 +9,9 @@ def palindrome (word):
     word = word.replace(",", "")
     return word == word[::-1]
 
-#Exercise 2: Balanced Parentheses
+# Exercise 2: Balanced Parentheses
+
+# First Attempt
 
 def parentheses(sequence):
     count = 0
@@ -21,3 +23,15 @@ def parentheses(sequence):
         if count < 0:
             return False
     return count == 0
+
+# Second Attempt
+
+def parentheses(sequence):
+    from collections import Counter
+
+    balance = 0
+    for par in sequence:
+        balance += Counter({'(': 1, ')': -1})[par]
+        if balance < 0:
+            return False
+    return balance == 0
